103e3b91817d43fabd52de8954c36c5ffd54664e
Mostafa Mohseni Kabir <mostafa_mohsenikabir@yahoo.com>
2018-02-14 11:09:05 +0330

    loglite: implement react-router-redux!

--- frontend/src/setup/redux.js
+++ frontend/src/setup/redux.js
@@ -1,20 +1,31 @@
 // modules
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { createEpicMiddleware, combineEpics } from 'redux-observable'
-// reducers and epics
-import AppEpic from '../components/Main/App.effect'
-import HomeEpic from '../components/components/Home/Main/Home.effect'
-import AddEpic from '../components/components/Add/Main/Add.effect'
-import ReportEpic from '../components/components/Report/Main/Report.effect'
+import { routerReducer, routerMiddleware } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
+// reducers
 import AppReducer from '../components/Main/App.reducer'
 import HomeReducer from '../components/components/Home/Main/Home.reducer'
 import AddReducer from '../components/components/Add/Main/Add.reducer'
 import ReportReducer from '../components/components/Report/Main/Report.reducer'
+// epics
+import AppEpic from '../components/Main/App.effect'
+import HomeEpic from '../components/components/Home/Main/Home.effect'
+import AddEpic from '../components/components/Add/Main/Add.effect'
+import ReportEpic from '../components/components/Report/Main/Report.effect'
+
+
+// Create a history of your choosing (we're using a browser history in this case)
+export const history = createHistory()
+
+// Build the middleware for intercepting and dispatching navigation actions
+const middleware = routerMiddleware(history)
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 /* eslint-enable */
 
+// redux observable
 const rootEpic = combineEpics(AppEpic, HomeEpic, AddEpic, ReportEpic)
 const epicMiddleware = createEpicMiddleware(rootEpic)
 
@@ -25,7 +36,8 @@ const store = createStore(
     Home: HomeReducer,
     Add: AddReducer,
     Report: ReportReducer,
-  }), composeEnhancers(applyMiddleware(epicMiddleware)))
+    router: routerReducer,
+  }), composeEnhancers(applyMiddleware(middleware, epicMiddleware)))
 
 
 export const { dispatch, getState } = store
