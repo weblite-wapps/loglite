6c67ad59a9162eec863d01cb4dc70605973da4c1
amirhossein <s.amir.s1372@gmail.com>
2018-09-12 11:09:45 +0430

    Merge branch dev into refactor-wis

--- backend/src/logic/components/tag/route.js
+++ backend/src/logic/components/tag/route.js
@@ -1,33 +1,32 @@
 // modules
-import * as R from 'ramda'
+import * as R from "ramda"
 // components
-import app from '../../../setup/server'
+import app from "../../../setup/server"
 // db helpers
-import { fetchTags, countTags, saveTag, updateTag } from './db'
+import { fetchTags, countTags, saveTag, updateTag } from "./db"
 // const
 const logger = console.log
 
-
-app.get('/fetchTags', ({ query: { wis, userId } }, res) =>
+app.get("/fetchTags", ({ query: { wis, userId } }, res) =>
   fetchTags({ wis, userId })
     .then(logs => res.json(logs))
-    .catch(logger))
+    .catch(logger),
+)
 
-
-app.get('/serachTags', ({ query: { wis, userId, label } }, res) =>
+app.get("/serachTags", ({ query: { wis, userId, label } }, res) =>
   fetchTags({ wis, userId, label: { $regex: `.*${label}.*` } })
     .then(tags => res.json(tags))
-    .catch(logger))
+    .catch(logger),
+)
 
-
-app.post('/saveTags', ({ body: { wis, userId, tags } }, res) => {
+app.post("/saveTags", ({ body: { wis, userId, tags } }, res) => {
   const addOrUpdateTag = tag =>
-    countTags({ wis, userId, label: tag })
-      .then((number) => {
-        if (number === 0) {
-          saveTag({ label: tag, number: 1, userId, wis })
-        } else updateTag({ label: tag }, { $inc: { number: 1 } })
-      })
+    countTags({ wis, userId, label: tag }).then(
+      number =>
+        number === 0
+          ? saveTag({ label: tag, number: 1, userId, wis })
+          : updateTag({ label: tag }, { $inc: { number: 1 } }),
+    )
   R.forEach(addOrUpdateTag, tags)
   fetchTags({ wis, userId })
     .then(fetchedTags => res.json(fetchedTags))
--- frontend/src/components/components/Report/components/Leaderboard/Leaderboard.selector.js
+++ frontend/src/components/components/Report/components/Leaderboard/Leaderboard.selector.js
@@ -1,15 +1,18 @@
 // modules
-import { createSelector } from 'reselect'
-import * as R from 'ramda'
+import { createSelector } from "reselect"
+import * as R from "ramda"
 // helpers
-import { getUsername } from './Leaderboard.helper'
-
+import { getUsername } from "./Leaderboard.helper"
 
 const getLeaderboard = state => state.Report.leaderboard
 
 const getLeaderboardData = createSelector(
   [getLeaderboard],
-  R.map(({ userId, score }) => ({ userId, username: getUsername(userId), score }))
+  R.map(({ userId, ...other }) => ({
+    userId,
+    username: getUsername(userId),
+    ...other,
+  })),
 )
 
 export { getLeaderboardData }
