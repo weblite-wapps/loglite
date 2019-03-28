6aba768ad939721814da9eda01071e885d567e0b
Masoud Salehi <msk.masoud.74@gmail.com>
2019-03-28 15:16:24 +0430

    chore(backend): Remove SSL; Change production port; Remove unused deps

--- backend/src/setup/prod.index.js
+++ backend/src/setup/prod.index.js
@@ -1,17 +1,7 @@
-// modules
-import path from 'path'
-import https from 'https'
-import fs from 'fs'
 // components
 import app from './server'
 import './mongodb'
 import '../logic/main/route'
 
 
-const privateKey = fs.readFileSync(path.resolve('./src/certs/express.key'), 'utf8')
-const certificate = fs.readFileSync(path.resolve('./src/certs/express.crt'), 'utf8')
-
-
-https
-  .createServer({ key: privateKey, cert: certificate }, app)
-  .listen(3080)
+app.listen(3081, () => console.log('Server Running!'))
